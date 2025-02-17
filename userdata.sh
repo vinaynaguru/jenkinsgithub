5aed9d6ed454fa059bdbdbd144b245c6674b611e
Vinay Kumar Naguru <vinay.naguru@encora.com>
2025-02-17 19:17:05 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install apache2 -y
+sudo apt install -y apache2
 echo "<h1>Welcome to My Web Server - Terraform & Jenkins</h1>" | sudo tee /var/www/html/index.html
-sudo systemctl enable apache2
-sudo systemctl start apache2
+sudo systemctl enable --now apache2
