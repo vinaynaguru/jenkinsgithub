7ddaacb8cba8bcb946428bdb57a02336d744d6eb
Vinay Kumar Naguru <vinay.naguru@encora.com>
2025-02-17 19:13:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
-  ami                    = "ami-0c55b159cbfafe1f0"  # Ubuntu 22.04 AMI
+  ami                    = "ami-080e1f13689e07408"  # Ubuntu 22.04 AMI (us-east-1)
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = file("userdata.sh")
 
   tags = {
-    Name = "Jenkins-WebServer"
+    Name = "JenkinsWebServer01"
   }
 }
